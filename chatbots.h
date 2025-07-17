9a2eef30bc65d148fb9e2294580bdf3ad04c944c
ktownsend756 <161529075+ktownsend756@users.noreply.github.com>
2025-07-17 06:51:29 -0700

    Update chatbots.h

--- chatbots.h
+++ chatbots.h
@@ -1,17 +1,13 @@
 #ifndef CHATBOTS_H
 #define CHATBOTS_H
 
-#include <stdio.h>
-#include <sys/types.h> //Fetches specific data types for
-#include <unistd.h> //POSIX access
 #include <semaphore.h>
-#include <fcntl.h> //Allows access to file control options
 #include <pthread.h>
 
 void create_file();
 sem_t* create_semaphore();
-void create_threads(pthread_t threads[], sem_t* sem);
 void* thread_function(void* arg);
+void create_threads(pthread_t threads[], sem_t* sem);
 void destroy_semaphore(sem_t *sem);
 
 #endif // CHATBOTS_H
