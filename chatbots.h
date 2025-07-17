3e847e56aa16008ca81d43d7fd1a887c4092f9a2
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-07-17 14:22:59 -0700

    Update chatbots.h

--- chatbots.h
+++ chatbots.h
@@ -4,10 +4,10 @@
 #include <semaphore.h>
 #include <pthread.h>
 
-void create_file();
-sem_t* create_semaphore();
-void* thread_function(void* arg);
-void create_threads(pthread_t threads[], sem_t* sem);
-void destroy_semaphore(sem_t *sem);
+void create_file(); // function to create the file
+sem_t* create_semaphore(); // function to create the semaphore
+void* thread_function(void* arg); // thread function for chat bots
+void create_threads(pthread_t threads[], sem_t* sem); // function to create the threads
+void destroy_semaphore(sem_t *sem); // function to destroy the semaphore
 
 #endif // CHATBOTS_H
