5ed58dda3773cb6369be21a6cc7359f8836aa6e2
ryanrenales <161529467+ryanrenales@users.noreply.github.com>
2025-07-17 14:27:51 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include "chatbots.h"
 
 int main(){
+    // create QUOTE.txt file
     create_file();
 
+    // create semaphore
     sem_t* sem = create_semaphore();
 
+    // create an array for 7 threads
     pthread_t threads[7];
+    // creates the 7 threads using the create_threads function
     create_threads(threads, sem);
 
+    // destroys the semaphore
     destroy_semaphore(sem);
 
     return 0;
